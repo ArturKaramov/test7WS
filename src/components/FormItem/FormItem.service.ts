48190c1a78e90ecf7781a4a38da0b403c3add088
Artur Karamov <karamov.1311@yandex.ru>
2024-06-20 14:23:00 +0300

    fix

--- src/components/FormItem/FormItem.service.ts
+++ src/components/FormItem/FormItem.service.ts
@@ -1,20 +1,20 @@
 import { IRow } from 'src/types/rows'
 
 export function calculateLength(arr: Array<IRow>, newRow: boolean): number {
-	if (!arr.length) {
-		return newRow ? 1 : 0
-	} else {
-		let res = 1
-		const queue = newRow ? [...arr, null] : [...arr]
+  if (!arr.length) {
+    return newRow ? 1 : 0
+  } else {
+    let res = 1
+    const stack = newRow ? [...arr, null] : [...arr]
 
-		while (queue.length > 1) {
-			const parent = queue.shift()
-			res += 1
-			if (parent && parent.child.length) {
-				queue.unshift(...parent.child)
-			}
-		}
+    while (stack.length > 1) {
+      const parent = stack.shift()
+      res += 1
+      if (parent && parent.child.length) {
+        stack.unshift(...parent.child)
+      }
+    }
 
-		return res
-	}
+    return res
+  }
 }
